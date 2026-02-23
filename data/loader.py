2fcf66260226b9d2f0f0ff546f451a29f207932e
Daksh1111174 <shahdakshinfo@gmail.com>
2026-02-23 17:21:22 +0530

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -1,10 +1,30 @@
 import yfinance as yf
+import pandas as pd
 
 def load_stock(ticker, start, end):
+
     df = yf.download(ticker, start=start, end=end, auto_adjust=True)
+
+    if df.empty:
+        return df
+
+    # Fix MultiIndex columns
+    if isinstance(df.columns, pd.MultiIndex):
+        df.columns = df.columns.get_level_values(0)
+
     df.reset_index(inplace=True)
+
     return df
 
+
 def load_portfolio(tickers, start, end):
-    df = yf.download(tickers, start=start, end=end)["Close"]
+
+    df = yf.download(tickers, start=start, end=end)
+
+    if df.empty:
+        return df
+
+    # Keep only Close
+    df = df["Close"]
+
     return df
